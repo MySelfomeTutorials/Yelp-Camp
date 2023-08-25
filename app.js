9290bb9c90a20a8f995527851bb9af3cc4358945
MySelfomeTutorials <myselfometutorials@gmail.com>
2023-08-25 18:35:27 +0530

    Yelp-Camp-Feat:Adding Basic Styles

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const morgan = require('morgan');
+const ejsMate = require('ejs-mate');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useNewUrlParser: true,
@@ -16,12 +18,13 @@ db.once('open', () => {
 	console.log('Database Connected');
 });
 
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
-
 
 app.get('/', async (req, res) => {
 	res.render('home');
@@ -49,17 +52,17 @@ app.get('/campgrounds/:id/edit', async (req, res) => {
 });
 
 app.put('/campgrounds/:id', async (req, res) => {
-	const {id}=req.params
-	const campground=await Campground.findByIdAndUpdate(id, {
-		...req.body.campground
-	})
-	res.redirect(`/campgrounds/${campground._id}`)
-})
+	const { id } = req.params;
+	const campground = await Campground.findByIdAndUpdate(id, {
+		...req.body.campground,
+	});
+	res.redirect(`/campgrounds/${campground._id}`);
+});
 app.delete('/campgrounds/:id', async (req, res) => {
 	const { id } = req.params;
-	await Campground.findByIdAndDelete(id)
-	res.redirect('/campgrounds')
-})
+	await Campground.findByIdAndDelete(id);
+	res.redirect('/campgrounds');
+});
 app.listen(3000, () => {
 	console.log('Serving on port 3000');
 });
